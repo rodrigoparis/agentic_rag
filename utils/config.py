3b421d644deb1b27db0f265d0017f4618ef701c2
rodrigoparis <rparisdv@gmail.com>
2025-04-27 22:52:11 -0300

    agentic_rag: adding friendly exit message

--- utils/config.py
+++ utils/config.py
@@ -5,5 +5,5 @@ load_dotenv()
 
 class Config:
     LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
-    KNOWLEDGE_FILE_PATH = os.getenv("KNOWLEDGE_FILE_PATH", "INdata/text.pdf")
+    KNOWLEDGE_FILE_PATH = os.getenv("KNOWLEDGE_FILE_PATH", "data/resume.pdf")
     HUGGINGFACEHUB_API_TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN")
